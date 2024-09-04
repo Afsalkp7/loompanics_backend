49784acb9420ad3a2d4795411ec1ad00bdfca04c
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-09-04 12:56:16 +0530

    hosting error

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import database from './database/connect.js'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import auth from './routes/auth.js'
-import adminAuth from './routes/adminAuth.js'
-import userRouter from './routes/userManagement.js'
-import authorsRouter from './routes/authorsRouter.js'
-import categoryRouter from './routes/categoryRoute.js'
-import publisherRouter from './routes/publisherRouter.js'
-import productRouter from './routes/productRouter.js'
+import express from 'express';
+import bodyParser from 'body-parser';
+import database from './database/connect.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import auth from './routes/auth.js';
+import adminAuth from './routes/adminAuth.js';
+import userRouter from './routes/userManagement.js';
+import authorsRouter from './routes/authorsRouter.js';
+import categoryRouter from './routes/categoryRoute.js';
+import publisherRouter from './routes/publisherRouter.js';
+import productRouter from './routes/productRouter.js';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 const corsOptions = {
-    origin: ['http://localhost:5173',], // List allowed origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-    credentials: true, // Allow cookies to be sent with requests
-  };
-app.use(cors(corsOptions))
+  origin: 'http://localhost:5173', // Exact URL of your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  credentials: true, // Allow cookies and other credentials
+  optionsSuccessStatus: 204, // For legacy browser support
+};
 
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Initialize the database
 database();
 
 // Routes
 app.use('/api/auth', auth);
-app.use('/api/adminAuth',adminAuth)
-app.use("/api/users",userRouter)
-app.use("/api/authors",authorsRouter)
-app.use("/api/categories",categoryRouter)
-app.use("/api/publishers",publisherRouter)
-app.use("/api/products",productRouter)
+app.use('/api/adminAuth', adminAuth);
+app.use('/api/users', userRouter);
+app.use('/api/authors', authorsRouter);
+app.use('/api/categories', categoryRouter);
+app.use('/api/publishers', publisherRouter);
+app.use('/api/products', productRouter);
 
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'connect' });
+});
 
-app.get('/',(req,res)=>{
-    res.status(200).json({msg:'connect'})
-})
-
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
