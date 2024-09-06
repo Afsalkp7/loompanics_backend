af44be2de18340305dd2fbc6c50c0349da919f21
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-09-06 23:47:57 +0530

    message section added

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { adminAuthMiddleware } from '../middlewares/adminAuthMiddleware.js';
 import { authMiddleware } from '../middlewares/authMiddlewares.js';
+import { addMessage } from '../controllers/contactUsCotroller.js';
 const router = express.Router();
 
-router.post("/",authMiddleware,)
+router.post("/",authMiddleware,addMessage)
 
 export default router;
