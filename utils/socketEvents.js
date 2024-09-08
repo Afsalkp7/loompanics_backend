03558f8084c197f5c9fb348443553bcdd0e5ee56
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-09-08 08:35:18 +0530

    socket configured

--- utils/socketEvents.js
+++ utils/socketEvents.js
@@ -1,14 +1,15 @@
-export default function socketEvents(io) {
+// socketEvents.js
+export default (io) => {
     io.on('connection', (socket) => {
       console.log('A user connected:', socket.id);
   
-      // Handle user joining a room
+      // Join room event (room should be user-specific, e.g., user ID)
       socket.on('joinRoom', (room) => {
         socket.join(room);
         console.log(`User joined room: ${room}`);
       });
   
-      // Handle receiving a message and broadcasting it
+      // Handle receiving a message and broadcasting it to the room
       socket.on('sendMessage', (message) => {
         io.to(message.room).emit('receiveMessage', message);
       });
@@ -18,5 +19,5 @@ export default function socketEvents(io) {
         console.log('User disconnected:', socket.id);
       });
     });
-  }
+  };
   
